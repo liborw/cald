6ceaa936070f20af9fcb8b408629ef27a6e3eac9
Libor Wagner <liborwagner@gmail.com>
2018-11-11 23:47:45 +0100

    Kind of big update

---
+++ README.md
@@ -0,0 +1,12 @@
+---
+title:  Cald API Library
+author: Libor Wagner <libor.wagner@cvut.cz>
+date:   2018-11-11
+---
+
+# Cald API Library
+
+ - Season ID: 16 - 2018, 17 - 2019
+ - Jelinek id: 1920
+
+
--- cald.py
+++ cald.py
@@ -3,6 +3,8 @@
 
 import requests
 import json
+import pprint
+import logging
 
 
 class CaldApi(object):
@@ -10,6 +12,7 @@ class CaldApi(object):
     def __init__(self, baseurl='http://cald.yosarin.net'):
         self.baseurl = baseurl
         self.token = None
+        self.log = logging.getLogger('CaldApi')
 
     def headers(self):
         if self.token is not None:
@@ -21,15 +24,23 @@ class CaldApi(object):
     def _post(self, addr, data={}):
         url = self.baseurl + addr
         data.update(self.headers())
-        req = requests.post(url, data=data)
-        content = json.loads(req.text)
+        req = requests.post(url, json=data)
+        try:
+            content = json.loads(req.text)
+        except json.JSONDecodeError:
+            self.log.warn()
+            return {'error': 'unable to parse json', 'text': req.text}
         return content
 
     def _get(self, addr, data={}):
         url = self.baseurl + addr
         data.update(self.headers())
-        req = requests.get(url, data=data)
-        content = json.loads(req.text)
+        req = requests.get(url, json=data)
+        try:
+            content = json.loads(req.text)
+        except json.JSONDecodeError:
+            self.log.warn()
+            return {'error': 'unable to parse json', 'text': req.text}
         return content
 
     def authorize(self, user, password):
@@ -39,59 +50,155 @@ class CaldApi(object):
             return (False, ret)
         else:
             self.token = ret['token']['token']
-            return (True, ret)
+            return (True, ret['token'])
 
     def user_me(self):
         ret = self._get('/user/me')
         return ret
 
+    def list(self, what, where=None, extend=None, limit=None, offset=None):
 
-##
+        data = dict()
+        if where is not None:
+            data['filter'] = where
+        if extend is not None:
+            data['extend'] = extend
+        if limit is not None:
+            data['limit'] = limit
+        if offset is not None:
+            data['offset'] = offset
 
-baseurl = 'http://cald.yosarin.net'
+        ret = self._get('/list/' + what, data=data)
 
-def get(url):
-    url = 'http://evidence.cald.cz/api/' + url
-    headers = {'X-Auth-Token': "9855f563851b600756496b976a14d60a2e99aceec5653c920526d1e10e34bc47"}
-    req = requests.get(url, headers=headers)
-    req = requests.get(url, headers=headers)
-    content = json.loads(req.text)
-    return content
+        if 'error' in ret:
+            return (False, ret)
+        else:
+            return (True, ret['data'])
 
+    def new_player(self, player):
+        self.log.debug('update_player: %s %s',
+                       player['first_name'],
+                       player['last_name'])
 
-def create_user(user, password, email):
-    url = baseurl + '/user'
-    data = {'email': email, 'password': password, 'login': user}
-    req = requests.post(url, data=data)
-    content = json.loads(req.text)
-    return content
+        player = dict(player)
 
+        if 'address' in player:
+            del player['address']
 
-def get_auth_token(user, password):
-    url = baseurl + '/user/login'
-    data = {'login': user, 'password': password}
-    req = requests.post(url, data=data)
-    content = json.loads(req.text)
-    return content['token']['token']
+        # new player
+        ret = self._post('/player', data=player)
+        self.log.debug('Resp: %s', str(ret))
 
-## authentication
+        if 'error' in ret:
+            return (False, ret)
 
-user = 'agentSunset'
-password = 'sunset2018'
+        pid = int(ret['id'])
+        return (True, pid)
 
-# create_user(user, password, 'test@test.cz')
+    def add_team_player(self, teamid, playerid, season):
+        self.log.debug('add_team_player teamid: %d, playerid: %d, seasonid: %d',
+                       teamid,
+                       playerid,
+                       season)
 
-resp = {'id': '67', 'info': 'User created', 'mail_send': True, 'status': 'OK'}
+        data = {'season_id': season}
+        ret = self._post('/team/' + str(teamid) + '/player/' + str(playerid), data)
+        self.log.debug('Resp: %s', str(ret))
 
+        if 'error' in ret:
+            return (False, ret)
+        else:
+            return (True, ret)
 
-resp = get_auth_token(user, password)
+    def update_player(self, player, pid):
+        self.log.debug('update_player: %s %s pid: %d',
+                       player['first_name'],
+                       player['last_name'],
+                       pid)
 
-##
+        player = dict(player)
 
-cald = CaldApi(baseurl)
-cald.authorize(user, password)
-cald.user_me()
+        if 'address' in player:
+            del player['address']
 
+        # update player
+        ret = self._post('/player/' + str(pid), data=player)
+        self.log.debug('Resp: %s', str(ret))
 
+        if 'error' in ret:
+            return (False, ret)
+        else:
+            return (True, ret['data'])
+
+    def update_team(self, team, teamid):
+        ret = self._post('/team/' + str(teamid))
+
+        if 'error' in ret:
+            return (False, ret)
+        else:
+            return (True, ret['data'])
+
+    def get_player_address(self, pid):
+        ret = self._get('/player/' + str(pid) + '/address')
+
+        if 'error' in ret:
+            return (False, ret)
+        else:
+            return (True, ret['data'])
+
+    def get_season_id(self, year):
+        ret, data = self.list('season', where={'name': '2018'})
+        if ret and data:
+            return int(data[0]['id'])
+        else:
+            return None
+
+    def add_player_address(self, address, pid):
+
+        if 'type' not in address:
+            self.log.warning('No address type, set deault')
+            address['type'] = 'permanent residence'
+
+        uri = '/player/{}/address'.format(pid)
+        ret = self._post(uri, data=address)
+
+        if 'error' in ret:
+            return (False, ret)
+        else:
+            return (True, ret)
+
+    def update_player_address(self, address, pid, aid):
+        uri = '/player/{}/address/{}'.format(pid, aid)
+        ret = self._post(uri, data=address)
+
+        if 'error' in ret:
+            return (False, ret)
+        else:
+            return (True, ret)
+
+    def get_player_address_id(self, pid):
+        ret, data = self.get_player_address(pid)
+
+        if ret and data:
+            return data[0]['id']
+        else:
+            return []
+
+    def get_team_players(self, teamid):
+        return self.list('player_at_team', where={'team_id': teamid})
+
+    def get_player(self, **kvargs):
+        return self.list('player', where=kvargs)
+
+    def get_team(self, **kvargs):
+        return self.list('team', where=kvargs)
+
+    def is_player_in_team(self, playerid, teamid, seasonid):
+        self.log.debug('is_player_in_team: pid: %d, tid: %d sid: %d',
+                playerid, teamid, seasonid)
+        query = {'player_id': playerid, 'team_id': teamid, 'season_id': seasonid}
+        ret, data = self.list('player_at_team', where=query)
+
+        return len(data) > 0
 
 
